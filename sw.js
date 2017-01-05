e6f29ffc35f30a14257f9772d92b48680857a48a
Tyler Nickerson <nickersoft@gmail.com>
2017-01-05 10:09:17 -0500

    Updated sw and Push

--- sw.js
+++ sw.js
@@ -1,24 +1,49 @@
+var isFunction = function (obj) {
+    return obj && {}.toString.call(obj) === '[object Function]';
+};
+
 self.addEventListener('message', function(event){
-    self.link = JSON.parse(event.data).link || '';
+    var data = JSON.parse(event.data);
+    console.log(data);
+    self.client = event.source;
+
+    if (data.link)
+        self.link = data.link;
+
+    if (data.id)
+        self.id = data.id;
 });
 
+self.onnotificationclose = function(event) {
+    /* Tell Push to execute close callback */
+    console.log(self.client);
+    self.client.postMessage('close');
+};
+
 self.onnotificationclick = function(event) {
-    event.notification.close();
+    if (self.link) {
+        event.notification.close();
 
-    // This looks to see if the current is already open and focuses if it is
-    event.waitUntil(clients.matchAll({
-        type: "window"
-    }).then(function(clientList) {
-        var client;
+        // This looks to see if the current is already open and focuses if it is
+        event.waitUntil(clients.matchAll({
+            type: "window"
+        }).then(function (clientList) {
+            var client;
 
-        for (var i = 0; i < clientList.length; i++) {
-            client = clientList[i];
+            for (var i = 0; i < clientList.length; i++) {
+                client = clientList[i];
 
-            if ((client.url == '/' + self.link) && ('focus' in client))
-                return client.focus();
-        }
+                if ((client.url == '/' + self.link) && ('focus' in client))
+                    return client.focus();
+            }
 
-        if (clients.openWindow)
-            return clients.openWindow('/' + self.link);
-    }));
+            if (clients.openWindow)
+                return clients.openWindow('/' + self.link);
+        }));
+    }
+
+    /* Tell Push to execute click callback */
+    console.log(self.client);
+    console.log(self.link);
+    self.client.postMessage('click');
 };
